b96dffc24d489c0f7210c6a256c06a853ab94f60
titouan <titouan.morvan1@gmail.com>
2021-12-27 21:41:43 +0100

    fix parsing

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ from sirenisation import sirenise
 if __name__ == "__main__":
 
     # fichier non consolidé de l'index égalité homme femme
-    egal_hf = pd.read_csv("data/")
+    egal_hf = pd.read_csv("data/index-egalite-fh.csv", sep=";")
 
     # sirenisation des entreprises
-    sirens = egal_hf[["SIREN"]]
+    sirens = set(egal_hf["SIREN"])
     sirenise(sirens)
 
     # conversion des codes de la nomenclature NAF2 vers 
-    sirens = pd.read("data/")
-    nomenc = pd.read_csv("data/")
+    sirens = pd.read_csv("data/sirens.csv")
+    nomenc = pd.read_csv("data/naf.csv")
     sirens = sirens.merge(nomenc, how="left", left_on="activite_principale", right_on="id_5")
 
     # ajout des informations à la base initiale
-    egal_hf_cons = egal_hf.merge(sirens, on="siren")
-    egal_hf_cons.to_csv("data/")
+    egal_hf_cons = egal_hf.merge(sirens, left_on="SIREN", right_on="siren")
+    egal_hf_cons.to_csv("data/index_eg_hf_cons.csv")
--- sirenisation.py
+++ sirenisation.py
@@ -1,29 +1,35 @@
 import requests
-import sleep
-import tqdm
+from time import sleep
+import pandas as pd
+from tqdm import tqdm
 
-API_URL = "https://entreprise.data.gouv.fr/api/sirene/v3/etablissements/"
-FIELDS = ['activite_principale','tranche_effectifs','categorie_entreprise','date_creation']
+API_URL = "https://entreprise.data.gouv.fr/api/sirene/v3/unites_legales/"
+FIELDS = ['siren', 'activite_principale', 'tranche_effectifs', 'categorie_entreprise', 'date_creation']
 
 def get_file(siren):
-    url = API_URL + siren
+    url = API_URL + str(siren)
     resp = requests.get(url).json()
+    resp = resp['unite_legale']
     return resp
 
-def parse(resp):  
+def parse(resp):
     row = {field:resp.get(field,"") for field in FIELDS}
-    row.update({'siren' : siren})
+    row["n_etablissements"] = len(resp['etablissements'])
     return row
 
 def sirenise(sirens):
     data = []
     for siren in tqdm(sirens):
-        resp = get_file(siren)  
-        row = parse(resp)
-        data.append(row)
-        time.sleep(0.2)
+        try :
+            resp = get_file(siren)
+        except KeyError:
+            continue
+        else:
+            row = parse(resp)  
+            data.append(row)
+        sleep(0.2)
     df = pd.DataFrame(data)
-    df.to_csv("data/siren.csv")
+    df.to_csv("data/sirens.csv", index = False)
 
 
 
