4133cb4fd3126ca600274c71b2b4082b9163a432
titouan <titouan.morvan1@gmail.com>
2022-04-03 13:47:50 +0200

    clean code

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 import pandas as pd
-from sirenisation import sirenise
 
+from sirenisation import sirenise
 
 if __name__ == "__main__":
 
     # fichier non consolidé de l'index égalité homme femme
-    egal_hf = pd.read_csv("data/index-egalite-fh.csv", sep=";")
+    egal_hf = pd.read_csv("data/index-egalite-fh.csv", sep=";", dtype={"SIREN": str})
 
     # sirenisation des entreprises
     sirens = set(egal_hf["SIREN"])
-    sirenise(sirens)
+    df_sirens = sirenise(sirens)
 
-    # conversion des codes de la nomenclature NAF2 vers 
-    sirens = pd.read_csv("data/sirens.csv")
+    # conversion des codes de la nomenclature NAF2 vers l'activité
     nomenc = pd.read_csv("data/naf.csv")
-    sirens = sirens.merge(nomenc, how="left", left_on="activite_principale", right_on="id_5")
+    df_sirens = df_sirens.merge(
+        nomenc, how="left", left_on="activite_principale", right_on="id_5"
+    )
 
     # ajout des informations à la base initiale
-    egal_hf_cons = egal_hf.merge(sirens, left_on="SIREN", right_on="siren")
+    egal_hf_cons = egal_hf.merge(df_sirens, left_on="SIREN", right_on="siren")
     egal_hf_cons.to_csv("data/index_eg_hf_cons.csv")
--- sirenisation.py
+++ sirenisation.py
@@ -1,38 +1,48 @@
-import requests
+import logging
 from time import sleep
+from typing import Dict, Any, Iterable, Union
+
 import pandas as pd
+import requests
 from tqdm import tqdm
 
+logging.basicConfig(filename="data/sirenisation.log", level=logging.INFO)
+
 API_URL = "https://entreprise.data.gouv.fr/api/sirene/v3/unites_legales/"
-FIELDS = ['siren', 'activite_principale', 'tranche_effectifs', 'categorie_entreprise', 'date_creation']
+FIELDS = [
+    "siren",
+    "activite_principale",
+    "tranche_effectifs",
+    "categorie_entreprise",
+    "date_creation",
+]
 
-def get_file(siren):
-    url = API_URL + str(siren)
-    resp = requests.get(url).json()
-    resp = resp['unite_legale']
-    return resp
 
-def parse(resp):
-    row = {field:resp.get(field,"") for field in FIELDS}
-    row["n_etablissements"] = len(resp['etablissements'])
+def parse(resp: Dict[str, Any]) -> Dict[str, Union[str, int]]:
+    row = {field: resp.get(field, "") for field in FIELDS}
+    row["n_etablissements"] = len(resp["etablissements"])
     return row
 
-def sirenise(sirens):
+
+def sirenise(sirens: Iterable[str]) -> pd.DataFrame:
+    """
+    Sirenisation of a list of Siren id using SIREN API.
+    """
     data = []
     for siren in tqdm(sirens):
-        try :
-            resp = get_file(siren)
-        except KeyError:
+        sleep(0.2)
+        resp = requests.get(url=API_URL + siren)
+        if resp.ok:
+            siren_file = resp.json()["unite_legale"]
+            row = parse(siren_file)
+            data.append(row)
+        elif resp.status_code == 404:
+            logging.warning("Siren number not found : %s", siren)
             continue
         else:
-            row = parse(resp)  
-            data.append(row)
-        sleep(0.2)
-    df = pd.DataFrame(data)
-    df.to_csv("data/sirens.csv", index = False)
+            resp.raise_for_status()
 
+    df_siren = pd.DataFrame(data)
+    df_siren.to_csv("data/sirens.csv", index=False)
 
-
-
-
-    
+    return df_siren
